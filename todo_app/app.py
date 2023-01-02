76350e109e16fe8b598b0d8f960a4dea875b41b9
Josh Purdy <j.d.h.purdy@gmail.com>
2023-01-02 17:31:17 +0000

    exercise complete

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, redirect, request
+from flask import render_template
 
 from todo_app.flask_config import Config
+from todo_app.data.session_items import get_items, add_item
+import sys
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -8,4 +11,9 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html', tasks=get_items())
+
+@app.route('/add', methods=['POST'])
+def add():
+    add_item(request.form.get('task_title'))
+    return redirect("/")
