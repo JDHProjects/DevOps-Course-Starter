bac0503c294b24013119e069d96f6ec149d8dff4
Josh Purdy <j.d.h.purdy@gmail.com>
2023-04-09 10:44:48 +0100

    changed code to not re-request all items from trello every change

--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -4,81 +4,87 @@ import os
 TRELLO_BASE = "https://api.trello.com/1"
 
 class Item:
-  def __init__(self, card):
-    self.id = card["id"]
-    self.name = card["name"]
-    self.status_id = card["idList"]
-    if self.status_id == os.getenv('TRELLO_NOT_STARTED_LIST'):
-      self.status = "Not Started" 
-    elif self.status_id == os.getenv('TRELLO_IN_PROGRESS_LIST'):
-      self.status = "In Progress"
-    else:
-      self.status = "Completed"
+    def __init__(self, card):
+      self.id = card["id"]
+      self.name = card["name"]
+      self.status_id = card["idList"]
+      if self.status_id == os.getenv('TRELLO_NOT_STARTED_LIST'):
+        self.status = "Not Started" 
+      elif self.status_id == os.getenv('TRELLO_IN_PROGRESS_LIST'):
+        self.status = "In Progress"
+      else:
+        self.status = "Completed"
 
-  def __str__(self):
-    return self.name
+    def __str__(self):
+      return self.name
 
-def build_params(**kwargs):
-  """
-  Creates param dict with auth.
+class Items:
+  def __init__(self):
+    self.items = self.get_items()
 
-  Returns:
-    dict: The built params dict.
-  """
-  params = kwargs
-  params['key'] = os.getenv('TRELLO_API_KEY')
-  params['token'] = os.getenv('TRELLO_TOKEN')
-  return params
+  def _build_params(self, **kwargs):
+    """
+    Creates param dict with auth.
 
-
-def get_items():
-  """
-  Fetches all saved items from the Trello board.
-
-  Returns:
-    list: The list of saved items.
-  """
-  response = requests.request(
-    "GET",
-    f'{TRELLO_BASE}/boards/{os.getenv("TRELLO_BOARD_ID")}/cards',
-    params=build_params()
-  )
-
-  return [Item(card) for card in response.json()]
+    Returns:
+      dict: The built params dict.
+    """
+    params = kwargs
+    params['key'] = os.getenv('TRELLO_API_KEY')
+    params['token'] = os.getenv('TRELLO_TOKEN')
+    return params
 
 
-def add_item(title):
-  """
-  Adds a new item with the specified title to the Trello board.
+  def get_items(self):
+    """
+    Fetches all saved items from the Trello board.
 
-  Args:
-    title: The title of the item.
+    Returns:
+      list: The list of saved items.
+    """
+    response = requests.request(
+      "GET",
+      f'{TRELLO_BASE}/boards/{os.getenv("TRELLO_BOARD_ID")}/cards',
+      params=self._build_params()
+    )
 
-  Returns:
-    item: The saved item.
-  """
-  response = requests.request(
-    "POST",
-    f"{TRELLO_BASE}/cards",
-    params=build_params(idList=os.getenv('TRELLO_NOT_STARTED_LIST'), name=title)
-  )
-
-  card = response.json()
-  return Item(card)
+    self.items = [Item(card) for card in response.json()]
+    return self.items
 
 
-def change_item_list(id, list_id):
-  """
-  Moves an existing item in the Trello board to be part of a different list.
+  def add_item(self, title):
+    """
+    Adds a new item with the specified title to the Trello board.
 
-  Args:
-    item: The item to update.
-  """
-  response = requests.request(
-    "PUT",
-    f'{TRELLO_BASE}/cards/{id}',
-    params=build_params(idList=list_id)
-  )
+    Args:
+      title: The title of the item.
+    """
+    response = requests.request(
+      "POST",
+      f"{TRELLO_BASE}/cards",
+      params=self._build_params(idList=os.getenv('TRELLO_NOT_STARTED_LIST'), name=title)
+    )
 
-  card = response.json()
-  return Item(card)
+    card = response.json()
+    self.items.append(Item(card))
+
+
+  def change_item_list(self, id, list_id):
+    """
+    Moves an existing item in the Trello board to be part of a different list.
+
+    Args:
+      item: The item to update.
+    """
+    response = requests.request(
+      "PUT",
+      f'{TRELLO_BASE}/cards/{id}',
+      params=self._build_params(idList=list_id)
+    )
+
+    card = response.json()
+    for item in self.items:
+      if item.id == id:
+        self.items.remove(item)
+        self.items.append(Item(card))
+        return 
